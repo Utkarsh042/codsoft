c570bf7fa54ed864dafebb9db1898daf7978f874
Utkarsh <singhutkarsh919@gmail.com>
2024-06-04 19:19:30 +0530

    test

---
+++ PORTFOLIO WEBSITE/main.js
@@ -0,0 +1,8 @@
+const hamburger = documnet.getElementById("hamburger")
+const navlist = document.getElementById("navlist")
+
+hamburger.addEventListener("click", ()=>{
+
+    navlist.classList.toggle("navlist-active");
+
+})
